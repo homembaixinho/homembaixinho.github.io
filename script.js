882c60927b47c6ddb47d52698c2fd16e49977b76
heitor <heitorleite.dev@gmail.com>
2020-07-29 19:34:09 -0300

    eliminate render-blocking resources

--- script.js
+++
@@ -1,9 +0,0 @@
-window.onload = () => {
-  let range = document.createRange();
-  let sel = window.getSelection();
-  let p = document.getElementById('email');
-
-  range.setStart(p.lastChild, p.lastChild.length); 
-  sel.removeAllRanges();
-  sel.addRange(range);
-}
