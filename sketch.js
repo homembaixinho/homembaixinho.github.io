5420394143583773b2ff17bacde8190b032b7d07
heitor <heitorleite.dev@gmail.com>
2021-05-09 20:42:57 -0300

    sketch rewrite

--- sketch.js
+++ sketch.js
@@ -1,82 +1,77 @@
-let scl = 50;
-let ypos = 0;
-let speed = 0.1;
-let w, h, r, rows, cols;
-
 function setup() { 
   createCanvas(windowWidth, windowHeight, WEBGL);
 
-  w = width * 2
-  h = height * 2
+  s = 50
+  speed = 0.03
+  cyan = color('#40dcfe')
+  pink = color('#ed03ff')
 
-  d = w/3
-  stripe = Math.floor(d/30)
-
-  rows = w/scl
-  cols = h/scl
+  w = 1.5*width
+  h = height
+  rows = w/s
+  cols = h/s+1
+  pos = 0
+  r = width/6
+  
+  heights = []  
+  for (let i = 0; i <= rows; i++)
+    heights[i] = []
 }
 
 function draw() { 
   background(0)
-  rotateX(PI / 2.5)
-
-  fill(0)
-  stroke(64, 220, 254);
-  strokeWeight(2)
-
-  push()
-  translate(-w/2, -h/2) 
-
-  for (let x = 0; x <= rows; x++) {
-    beginShape(TRIANGLE_STRIP)
-    for (let y = 0; y <= cols; y ++) {
-      let amp, nextAmp
-
-      if (x < 1/3 * rows -1 || x > 2/3 * rows) {
-        amp = nextAmp = 100
-      } else if (x < 1/3 * rows) {
-        amp = 100
-        nextAmp = 10
-      } else if (x > 2/3 * rows -1) {
-        amp = 10
-        nextAmp = 100
-      } else {
-        amp = nextAmp = 10
-      } 
-
-      vertex(x*scl, y*scl, map(noise(x, ypos + y*0.2), 0, 1, -amp, amp))
-      vertex((x+1)*scl, y*scl, map(noise(x+1, ypos + y*0.2), 0, 1, -nextAmp, nextAmp))
-    }
-    endShape()
-  }
-
-  ypos -= speed
-  pop()
-
-  push()
-  translate(0, -1.5*h/2)
-  rotateX(-PI/2.5)
   
-  stroke(237, 3, 255)
-  fill(237, 3, 255)
-  ellipse(0, 0, d)
-
-  stroke(0)
-  strokeWeight(10)
-  for (let y = stripe; y >= -d/2; y-= stripe) {
-    line(-d/2, y, d/2, y)
+  for (let i = 0; i <= rows; i++) {
+    for (let j = 0; j <= rows; j++) {
+      heights[i][j] =
+        sin(map(abs(i-rows/2), 0, rows/2, 0, HALF_PI)) *
+        map(noise(i, pos - j*0.2), 0, 1, 0, 200)
+    }
   }
+
+  pos += speed
+
+  push()
+    rotateX(PI/3)
+    translate(-w/2, -h/2)
+    strokeWeight(1)
+    stroke(cyan)
+    fill(0)
+  
+    for (let j = 0; j <= cols; j++) {
+      beginShape(TRIANGLE_STRIP)
+      for (let i = 0; i <= rows; i++) {
+        vertex(i*s, j*s, heights[i][j])
+        vertex(i*s, (j+1)*s, heights[i][j+1])
+      }
+      endShape();
+    }
+  pop()
+  
+  push()
+    rotateX(5/6*PI)
+    translate(0, h/16, h/2)
+  
+    stroke(pink)
+    fill(pink)
+    ellipse(0, 0, r*2)
+  
+    stroke(0)
+    strokeWeight(2)
+    for (let i = -r; i <= r; i += r/15) {
+      line(-r, i, r, i)
+    }
+  
   pop()
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   
-  w = width * 2
-  h = height * 2
-  
-  d = w/3
-
-  rows = w/scl
-  cols = h/scl
+  w = 1.5*width
+  h = height
+  rows = w/s
+  cols = h/s+1
+  pos = 0
+  r = width/6
 }
